f6ab7fcb7589b58ef7f25c6d56c5ee6a63f803d0
Matías <crosserain@gmail.com>
2024-01-30 23:44:23 -0300

    educational and general info components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import '../styles/App.css'
+import InfoGeneral from '../components/InfoGeneral.jsx';
+import EducationInfo from '../components/EducationExp.jsx';
 
 function App() {
-  
+    return(
+        <>
+            <InfoGeneral />
+            <EducationInfo />
+        </>
+    )
 }
 
-export default App
+export default App;
