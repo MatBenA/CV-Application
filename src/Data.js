1c2c7934a39057f62ad69e68968b166f129bf805
Matías <crosserain@gmail.com>
2024-01-31 10:04:56 -0300

    starting data file

---
+++ src/Data.js
@@ -0,0 +1,10 @@
+export const requiredInputs = {
+    generalInfo: [
+        { name: "firstname", label: "First Name ", type: "text" },
+        { name: "lastname", label: "Last Name ", type: "text" },
+        { name: "email", label: "Email ", type: "email" },
+        { name: "phone", label: "Phone ", type: "tel" },
+    ],
+    educationInfo: [],
+    profesionalInfo: [],
+};
