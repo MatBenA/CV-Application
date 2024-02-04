5d302091de1c5ac7dc827d22fe76c0377fcab510
Matías <crosserain@gmail.com>
2024-02-03 22:07:51 -0300

    soy un boludo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import '../styles/App.css'
+import "../styles/App.css";
+//import {requiredInputs} from "./Form.js";
+
+const requiredInputs = [
+    { name: "firstname", label: "First Name ", type: "text", id: 1 },
+    { name: "lastname", label: "Last Name ", type: "text", id: 2 },
+    { name: "email", label: "Email ", type: "email", id: 3 },
+    { name: "phone", label: "Phone ", type: "tel", id: 4 },
+]
 
 function App() {
-  
+    return (
+        <p>
+            {requiredInputs.map((index) => {
+                return(<p key={index}>hola</p>)
+            })}
+        </p>
+    );
 }
 
-export default App
+export default App;
