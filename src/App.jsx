631c6124f08acdead89c783d03c31cae382d0144
Matías <crosserain@gmail.com>
2024-02-04 13:50:00 -0300

    correct inputs

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,26 @@ import FormInput from "../components/FormInput.jsx";
 function App() {
     return (
         <>
-            {requiredInputs.map((section, index) => {
-                return (
-                    <section key={index}>
-                        {section.map((input) => {
-                            return (
-                                <div key={input.id}>
-                                    <FormInput
-                                        name={input.name}
-                                        label={input.label}
-                                        type={input.type}
-                                    />
-                                </div>
-                            );
-                        })}
-                    </section>
-                );
-            })}
+            <form>
+                {requiredInputs.map((section, index) => {
+                    return (
+                        <section key={index}>
+                            {section.map((input) => {
+                                return (
+                                    <div key={input.id}>
+                                        <FormInput
+                                            name={input.name}
+                                            label={input.label}
+                                            type={input.type}
+                                        />
+                                    </div>
+                                );
+                            })}
+                        </section>
+                    );
+                })}
+                <button type="submit">Submit!</button>
+            </form>
         </>
     );
 }
